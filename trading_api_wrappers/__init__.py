59f8454c0f55fe5ee716336a9528bf24cb00c211
Snake575 <sarang575@gmail.com>
2017-05-20 04:58:31 -0400

    Added Bitcoinity (non documented api)

--- trading_api_wrappers/__init__.py
+++ trading_api_wrappers/__init__.py
@@ -1,9 +1,11 @@
+from .bitcoinity import Bitcoinity
 from .bitfinex import BitfinexAuth, BitfinexPublic
 from .coindesk import CoinDesk
 from .kraken import Kraken
 from .surbtc import SURBTC
 
 __all__ = [
+    Bitcoinity,
     BitfinexAuth,
     BitfinexPublic,
     CoinDesk,
