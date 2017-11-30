a279849a94f26b1ae0674d808e72056ec7ad43f9
Daniel Pérez Rada <dperezrada@gmail.com>
2017-11-30 16:38:38 -0300

    ADD: Standard deposits and withdrawals for Surbtc

--- trading_api_wrappers/surbtc/client_standard.py
+++ trading_api_wrappers/surbtc/client_standard.py
@@ -1,12 +1,18 @@
 from ..base import StandardClient
-from .client_public import SURBTCPublic
+from .client_auth import SURBTCAuth
 
 class SURBTCStandard(StandardClient):
-    client = SURBTCPublic()
+    def __init__(self, key=False, secret=False, timeout=30):
+        StandardClient.__init__(self)
+        self.client = SURBTCAuth(key=str(key), secret=str(secret), timeout=timeout)
 
     @staticmethod
     def get_pair_mapping(base, quote):
         return base + '-' + quote
+
+    @staticmethod
+    def get_currency_mapping(currency):
+        return currency.upper()
 
     @classmethod
     def standarize_orderbook(cls, raw_orderbook):
@@ -21,3 +27,29 @@ class SURBTCStandard(StandardClient):
         orderbook['bids'] = tmp_orderbook.bids
         orderbook['asks'] = tmp_orderbook.asks
         return self.standarize_orderbook(orderbook)
+
+    def get_withdrawals(self, currency):
+        withdrawals = self.client.withdrawals(
+            currency=self.get_currency_mapping(currency)
+        )
+        return [
+            (
+                wdraw.id, wdraw.created_at.isoformat(), wdraw.state,
+                currency, wdraw.amount.amount, wdraw.data.address,
+                wdraw.data.tx_hash, wdraw.fee.amount
+            )
+            for wdraw in withdrawals
+        ]
+
+    def get_deposits(self, currency):
+        deposits = self.client.deposits(
+            currency=self.get_currency_mapping(currency)
+        )
+        return [
+            (
+                dep.id, dep.created_at.isoformat(), dep.state,
+                currency, dep.amount.amount, dep.data.address,
+                dep.data.tx_hash, dep.fee.amount
+            )
+            for dep in deposits
+        ]
