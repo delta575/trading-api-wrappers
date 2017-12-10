55abdf00b31bb264c85814da7ea9d4018e74a516
Daniel Pérez Rada <dperezrada@gmail.com>
2017-12-10 20:13:38 -0300

    WIP: Bitfinex Widthdrawal and deposits

--- trading_api_wrappers/bitfinex/client_standard.py
+++ trading_api_wrappers/bitfinex/client_standard.py
@@ -1,12 +1,19 @@
+from datetime import datetime
 from ..base import StandardClient
-from .client_public_v1 import BitfinexPublic
+from .client_auth_v1 import BitfinexAuth
 
 class BitfinexStandard(StandardClient):
-    client = BitfinexPublic()
+    def __init__(self, key=False, secret=False, timeout=30):
+        StandardClient.__init__(self)
+        self.client = BitfinexAuth(key=str(key), secret=str(secret), timeout=timeout)
+
+    @classmethod
+    def get_pair_mapping(cls, base, quote):
+        return cls.get_currency_mapping(base) + cls.get_currency_mapping(quote)
 
     @staticmethod
-    def get_pair_mapping(base, quote):
-        return base + quote
+    def get_currency_mapping(currency):
+        return currency.lower()
 
     @classmethod
     def standarize_orderbook(cls, raw_orderbook):
@@ -18,3 +25,36 @@ class BitfinexStandard(StandardClient):
         market = self.get_pair_mapping(base, quote)
         orderbook = self.client.order_book(market)
         return self.standarize_orderbook(orderbook)
+
+    def format_movements(self, row):
+        return (
+            "bitfinex-w-%s" % row['id'],
+            datetime.fromtimestamp(row['timestamp_created']).isoformat(),
+            "bitfinex",
+            row['id'],
+            row['status'],
+            currency,
+            row['amount'],
+            row.get('address', ''),
+            row.get('txid', ''),
+            row.get('fee', 0),
+        )
+    def get_withdrawals(self, currency):
+        withdrawals = self.client.movements(
+            currency=self.get_currency_mapping(currency)
+        )
+        return [
+            self.format_movements(wdraw)
+            for wdraw in withdrawals
+            if wdraw['type'] == 'WITHDRAWAL'
+        ]
+
+    def get_deposits(self, currency):
+        deposits = self.client.deposits(
+            currency=self.get_currency_mapping(currency)
+        )
+        return [
+            self.format_movements(wdraw)
+            for wdraw in withdrawals
+            if wdraw['type'] == 'DEPOSIT'
+        ]
