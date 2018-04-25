7953921b55390c94f28e520516f1d03f66cdb432
Snake575 <sarang575@gmail.com>
2018-04-25 12:30:20 -0300

    Fix Buda host param

--- trading_api_wrappers/buda/server.py
+++ trading_api_wrappers/buda/server.py
@@ -3,14 +3,15 @@ from ..base import Server
 # API Server
 PROTOCOL = 'https'
 HOST = 'www.buda.com/api'
-TEST_HOST = 'stg.buda.com/api'
+TEST_HOST = 'stg.surbtc.com/api'  # TODO: change to buda.com
 VERSION = 'v2'
 
 
 # Buda API server
 class BudaServer(Server):
 
-    def __init__(self, test, host=HOST):
+    def __init__(self, test, host=None):
+        host = host or HOST
         if test:
             host = TEST_HOST
         super().__init__(PROTOCOL, host, VERSION)
