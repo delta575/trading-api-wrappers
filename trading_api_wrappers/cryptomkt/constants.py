d8a87733f5c9539dba8a67c8f104114c9270b62f
Snake575 <sarang575@gmail.com>
2018-05-31 13:08:02 -0400

    Add new BTC and XLM markets on CryptoMKT

--- trading_api_wrappers/cryptomkt/constants.py
+++ trading_api_wrappers/cryptomkt/constants.py
@@ -7,9 +7,11 @@ ORDERS_LIMIT = 100
 class Currency(_Currency):
     ARS = dict(value='ARS')
     BRL = dict(value='BRL')
+    BTC = dict(value='BTC')
     CLP = dict(value='CLP')
-    ETH = dict(value='ETH', decimals=18)
+    ETH = dict(value='ETH', decimals=9)
     EUR = dict(value='EUR')
+    XLM = dict(value='XLM')
 
 
 class OrderType(_Enum):
@@ -18,9 +20,18 @@ class OrderType(_Enum):
 
 
 class Market(_Market):
+    BTC_ARS = dict(value='BTCARS', base=Currency.BTC, quote=Currency.ARS)
+    BTC_BRL = dict(value='BTCBRL', base=Currency.BTC, quote=Currency.BRL)
+    BTC_CLP = dict(value='BTCCLP', base=Currency.BTC, quote=Currency.CLP)
+    BTC_EUR = dict(value='BTCEUR', base=Currency.BTC, quote=Currency.EUR)
     ETH_ARS = dict(value='ETHARS', base=Currency.ETH, quote=Currency.ARS)
+    ETH_BRL = dict(value='ETHBRL', base=Currency.ETH, quote=Currency.BRL)
     ETH_CLP = dict(value='ETHCLP', base=Currency.ETH, quote=Currency.CLP)
     ETH_EUR = dict(value='ETHEUR', base=Currency.ETH, quote=Currency.EUR)
+    XLM_ARS = dict(value='XLMARS', base=Currency.XLM, quote=Currency.ARS)
+    XLM_BRL = dict(value='XLMBRL', base=Currency.XLM, quote=Currency.BRL)
+    XLM_CLP = dict(value='XLMCLP', base=Currency.XLM, quote=Currency.CLP)
+    XLM_EUR = dict(value='XLMEUR', base=Currency.XLM, quote=Currency.EUR)
 
 
 class TimeFrame(_Enum):
